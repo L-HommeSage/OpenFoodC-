21f396c44c0712093cbbaad705a45bd4da65cffe
L-HommeSage <L-HommeSage24@users.noreply.github.com>
2020-02-03 14:41:12 +0100

    add people to view model

---
+++ OpenFood-C-Sharp/OpenFood-C-Sharp/ViewModel/PeopleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenFood_C_Sharp.ViewModel
+{
+    class PeopleViewModel
+    {
+    }
+}
