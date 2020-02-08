0705a7e89aed0eed69f6566b876e3899515873d3
L-HommeSage <L-HommeSage24@users.noreply.github.com>
2020-02-08 10:47:53 +0100

    add classes

--- OpenFood-C-Sharp/OpenFood-C-Sharp/Modele/Film.cs
+++ OpenFood-C-Sharp/OpenFood-C-Sharp/Modele/Film.cs
@@ -8,15 +8,19 @@ namespace OpenFood_C_Sharp.Modele
 {
     class Film
     {
-        public string title { get; set; }
-        public int episode_id { get; set; }
-        public string opening_crawl { get; set; }
+        public List<string> characters { get; set; }
+        public string created { get; set; }
         public string director { get; set; }
+        public string edited { get; set; }
+        public string episode_id { get; set; }
+        public string opening_crawl { get; set; }
+        public List<string> planets { get; set; }
         public string producer { get; set; }
         public string release_date { get; set; }
         public List<string> species { get; set; }
-        public List<string> characters { get; set; }
-        
+        public List<string> starships { get; set; }
+        public string title { get; set; }
+        public List<string> vehicles { get; set; }
 
     }
 }
--- OpenFood-C-Sharp/OpenFood-C-Sharp/Modele/People.cs
+++ OpenFood-C-Sharp/OpenFood-C-Sharp/Modele/People.cs
@@ -6,16 +6,24 @@ using System.Threading.Tasks;
 
 namespace OpenFood_C_Sharp.Modele
 {
-   public class People
+    class People
     {
-        public string name { get; set; }
-        public string hairColor { get; set; }
-        public string height { get; set; }
-        public string mass { get; set; }
-        public string skinColor { get; set; }
-        public List<string> vehicles { get; set; }
+        public string birth_year { get; set; }
+        public string eye_color { get; set; }
         public List<string> films { get; set; }
-        
+        public string gender { get; set; }
+        public string hair_color { get; set; }
+        public string height { get; set; }
+        public string homeworld { get; set; }
+        public string mass { get; set; }
+        public string name { get; set; }
+        public string skin_color { get; set; }
+        public string created { get; set; }
+        public string edited { get; set; }
+        public List<string> species { get; set; }
+        public List<string> starships { get; set; }
+        public List<string> vehicles { get; set; }
+
     }
  
 }
