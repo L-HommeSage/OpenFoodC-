395dd18cf6835599cb8e1b351b974724a186497f
L-HommeSage <L-HommeSage24@users.noreply.github.com>
2020-02-08 12:54:18 +0100

    add viewmodel

--- OpenFood-C-Sharp/OpenFood-C-Sharp/ViewModel/StarshipViewModel.cs
+++ OpenFood-C-Sharp/OpenFood-C-Sharp/ViewModel/StarshipViewModel.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using OpenFood_C_Sharp.Modele;
+using Newtonsoft.Json;
 
 namespace OpenFood_C_Sharp.ViewModel
 {
     class StarshipViewModel
     {
+        public static Starship GetStarship(string url)
+        {
+            HttpWebRequest request = HttpWebRequest.CreateHttp(url);
+            string responseStreamReader;
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                {
+                    responseStreamReader = reader.ReadToEnd();
+                }
+            }
+            return JsonConvert.DeserializeObject<Starship>(responseStreamReader);
+        }
+
+        public static List<Starship> GetAllStarship()
+        {
+            HttpWebRequest request = HttpWebRequest.CreateHttp("https://swapi.co/api/starships/");
+            string responseStreamReader;
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                {
+                    responseStreamReader = reader.ReadToEnd();
+                }
+            }
+            return JsonConvert.DeserializeObject<List<Starship>>(responseStreamReader);
+
+        }
     }
 }
