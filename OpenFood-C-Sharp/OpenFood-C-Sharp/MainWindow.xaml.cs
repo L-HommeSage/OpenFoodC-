7f07b762faadb05142e3bbcd57d7392f7588a638
AlexandreLamb <alexandre.lambert@edu.ece.fr>
2020-02-26 12:13:38 +0100

    add people page on double click

--- OpenFood-C-Sharp/OpenFood-C-Sharp/MainWindow.xaml.cs
+++ OpenFood-C-Sharp/OpenFood-C-Sharp/MainWindow.xaml.cs
@@ -44,7 +44,8 @@ namespace OpenFood_C_Sharp
             {
                 case "OpenFood_C_Sharp.Modele.People":
                  People p = (People)ListElements.SelectedItem;
-                this.Navigate(new Uri("HomePage.xaml", UriKind.Relative));
+                    PeoplePage peoplePage = new PeoplePage(p.url);
+                    this.Content = peoplePage;
                     break;
                 case "OpenFood_C_Sharp.Modele.Film":
                    Film f = (Film)ListElements.SelectedItem;
