2fcba348927877309b9b2175273faa82515a8fa2
L-HommeSage <L-HommeSage24@users.noreply.github.com>
2020-03-22 19:05:55 +0100

    toutes les pages faites sauf la page film

--- OpenFood-C-Sharp/OpenFood-C-Sharp/View/PeoplePage.xaml.cs
+++ OpenFood-C-Sharp/OpenFood-C-Sharp/View/PeoplePage.xaml.cs
@@ -27,9 +27,55 @@ namespace OpenFood_C_Sharp
             InitializeComponent();
             People people = PeopleViewModel.GetPeople(url);
             name.Content += people.name;
-            mass.Content += people.mass;
-            height.Content += people.height;
+            mass.Content += ' '+people.mass;
+            height.Content += ' ' + people.height;
+            birth.Content += ' ' + people.birth_year;
+            eye.Content += ' ' + people.eye_color;
+            gender.Content += ' ' + people.gender;
+            hair.Content += ' ' + people.hair_color;
+            homeworld.Content += ' ' + GetHomeWorld(people.homeworld);
+            skin.Content += ' ' + people.skin_color;
+            created.Content += ' ' + ConvertToDateTime(people.created);
+            edited.Content += ' ' + ConvertToDateTime(people.edited);
 
+
+            listFilms.Items.Clear();
+            listStarships.Items.Clear();
+            listVehicles.Items.Clear();
+            foreach (String f in people.films)
+            {
+                listFilms.Items.Add(FilmViewModel.GetFilm(f));
+            }
+            foreach (String s in people.starships)
+            {
+                listStarships.Items.Add(StarshipViewModel.GetStarship(s));
+            }
+            foreach (String v in people.vehicles)
+            {
+                listVehicles.Items.Add(VehicleViewModel.GetVehicle(v));
+            }
+        }
+        private static string ConvertToDateTime(string value)
+        {
+            DateTime convertedDate;
+            try
+            {
+                convertedDate = Convert.ToDateTime(value);
+                string input = value;
+                string sub = input.Substring(0, 10);
+
+                return (sub);
+            }
+            catch (FormatException)
+            {
+                return(value);
+            }
+        }
+
+        private static string GetHomeWorld(string value)
+        {
+            Planet planet = PlanetViewModel.GetPlanet(value);
+            return planet.name;
         }
     }
 }
